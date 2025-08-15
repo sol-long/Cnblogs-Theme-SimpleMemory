8a938ec99daf3de47525a54f388528078f23538e
wangyang <i@oyo.cool>
2025-08-15 13:50:28 +0800

    refactor(postMeta): 重构帖子元数据解析逻辑以提高可读性
    使用数组存储正则表达式并通过reduce方法匹配最佳结果，简化了代码结构

--- src/components/postMeta/postMeta.js
+++ src/components/postMeta/postMeta.js
@@ -7,30 +7,26 @@
  */
 
 export default function main(postDescText) {
-    let getPostMetaInfo = (postDescText) => {
-        postDescText = postDescText.replace(/[\r\n]/g, '');
-
-        let postMetaRex  = /.*posted\s*@\s*([0-9\-:\s]{16}).*阅读\s*\(([0-9]*)\).*评论\s*\(([0-9]*)\).*推荐\s*\(([0-9]*)\).*/,
-            postMetaRex2 = /.*posted\s*@\s*([0-9\-:\s]{16}).*阅读\s*\(([0-9]*)\).*评论\s*\(([0-9]*)\).*/,
-            postMetaRex3 = /.*posted\s*@\s*([0-9\-:\s]{16}).*/,
-            diggCount = $('#digg_count'),
-            info = postDescText.match(postMetaRex)
-                || postDescText.match(postMetaRex2)
-                || postDescText.match(postMetaRex3),
-            date = typeof info[1] === 'undefined' ? '1970-01-01 00:00' : info[1],
-            vnum = typeof info[2] === 'undefined' ? '0' : info[2],
-            cnum = typeof info[3] === 'undefined' ? '0' : info[3],
-            tnum = typeof info[4] === 'undefined' ?
-                (diggCount.length ? diggCount.text() : '0')
-                : info[4];
-
-        return {
-            date: date,
-            vnum: vnum,
-            cnum: cnum,
-            tnum: tnum,
-        };
+    const postDescTextClean = postDescText.replace(/[\r\n]/g, '');
+    
+    const regexps = [
+        /.*posted\s*@\s*([0-9\-:\s]{16}).*阅读\s*\((\d*)\).*评论\s*\((\d*)\).*推荐\s*\((\d*)\).*/,
+        /.*posted\s*@\s*([0-9\-:\s]{16}).*阅读\s*\((\d*)\).*评论\s*\((\d*)\).*/,
+        /.*posted\s*@\s*([0-9\-:\s]{16}).*/,
+    ];
+    
+    const match = regexps.reduce((bestMatch, regexp) => {
+        const currentMatch = postDescTextClean.match(regexp);
+        return currentMatch && currentMatch.length > bestMatch.length ? currentMatch : bestMatch;
+    }, []);
+    
+    const [, date, vnum, cnum, tnum] = match;
+    const diggCount = $('#digg_count');
+    
+    return {
+        date: date || '1970-01-01 00:00',
+        vnum: vnum || '0',
+        cnum: cnum || '0',
+        tnum: tnum || (diggCount.length ? diggCount.text() : '0'),
     };
-
-    return getPostMetaInfo(postDescText);
 }
